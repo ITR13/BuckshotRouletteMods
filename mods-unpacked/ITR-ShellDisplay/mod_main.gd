fecfb78e2dcde20519c154c3ced1f2221722c62c
ITR <ITR@live.no>
2024-02-23 00:18:42 +0100

    Converted repo to contain multiple mods. Added Shell Display Mod

---
+++ mods-unpacked/ITR-ShellDisplay/mod_main.gd
@@ -0,0 +1,26 @@
+extends Node
+
+const AUTHORNAME_MODNAME_DIR := "ITR-ShellDisplay"
+const AUTHORNAME_MODNAME_LOG_NAME := "ITR-ShellDisplay:Main"
+
+@export var shownLive : Array[Node]
+@export var shownBlank : Array[Node]
+
+var mod_dir_path := ""
+
+const ShellSpawner = preload("res://scripts/ShellSpawner.gd") 
+const ShellDisplay = preload("res://mods-unpacked/ITR-ShellDisplay/shell_display.gd") 
+
+var shellSpawner: ShellSpawner = null
+var shell_display : ShellDisplay
+var searchTimer: float = 0.1
+
+func _init() -> void:
+	mod_dir_path = ModLoaderMod.get_unpacked_dir()+(AUTHORNAME_MODNAME_DIR)+"/"
+
+func _ready() -> void:
+	var shell_display_prefab = load("res://mods-unpacked/ITR-ShellDisplay/shell_display.tscn")
+	shell_display = shell_display_prefab.instantiate()
+	add_child(shell_display)
+
+	
---
+++ mods-unpacked/ITR-ShellDisplay/shell_display.gd
@@ -0,0 +1,51 @@
+extends Control
+
+@export var shownLive : Array[Node]
+@export var shownBlank : Array[Node]
+
+var mod_dir_path := ""
+
+const ShellSpawner = preload("res://scripts/ShellSpawner.gd") 
+var shellSpawner: Node = null
+
+var searchTimer: float = 0.1
+var pastSequenceArrayLength: int = 8
+
+func _process(dt: float) -> void:
+	if shellSpawner == null:
+		MaybeFindShellSpawner(dt)
+	else:
+		MaybeUpdateShells()
+
+func MaybeFindShellSpawner(dt: float) -> void:
+	searchTimer -= dt
+	if searchTimer > 0:
+		return
+	searchTimer += 1
+	
+	shellSpawner = get_tree().get_root().get_node_or_null("main/standalone managers/shell spawner")
+	pastSequenceArrayLength = 8
+	for liveIndex in shownLive.size():
+		shownLive[liveIndex].visible = false
+	for blankIndex in shownBlank.size():
+		shownBlank[blankIndex].visible = false
+
+func MaybeUpdateShells() -> void:
+	if pastSequenceArrayLength == len(shellSpawner.sequenceArray):
+		return
+	
+	pastSequenceArrayLength = len(shellSpawner.sequenceArray)
+
+	var live = 0
+	var blank = 0
+	for shell in shellSpawner.sequenceArray:
+		if shell == "live":
+			live += 1
+		elif shell == "blank":
+			blank += 1
+	
+	for liveIndex in shownLive.size():
+		shownLive[liveIndex].visible = liveIndex < live
+	for blankIndex in shownBlank.size():
+		shownBlank[blankIndex].visible = blankIndex < blank
+	

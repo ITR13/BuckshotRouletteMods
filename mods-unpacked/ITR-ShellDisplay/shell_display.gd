8e693003dd6baf89da9a9d63786b6fc98000d2f4
ITR <ITR@live.no>
2024-06-09 18:46:04 +0200

    Shell Display 1.0.1
    fix #11 Images now use white and black shell pngs if color blind mode is on.
    fix #12 Display now goes away when you go back to main menu.

--- mods-unpacked/ITR-ShellDisplay/shell_display.gd
+++ mods-unpacked/ITR-ShellDisplay/shell_display.gd
@@ -2,6 +2,11 @@ extends Control
 
 @export var shownLive : Array[Node]
 @export var shownBlank : Array[Node]
+@export var shownLiveCb : Array[Node]
+@export var shownBlankCb : Array[Node]
+
+@export var cbShells : Node
+@export var otherShells : Node
 
 var mod_dir_path := ""
 
@@ -10,6 +15,8 @@ var shellSpawner: Node = null
 
 var searchTimer: float = 0.1
 var pastSequenceArrayLength: int = 8
+
+var colorblind: bool
 
 func _process(dt: float) -> void:
 	if shellSpawner == null:
@@ -25,18 +32,32 @@ func MaybeFindShellSpawner(dt: float) -> void:
 
 	var tree = get_tree()
 	if tree.current_scene.name != "main":
+		cbShells.visible = false;
+		otherShells.visible = false;
 		return
 
 	shellSpawner = tree.get_root().get_node_or_null("main/standalone managers/shell spawner")
 	pastSequenceArrayLength = 8
 	for liveIndex in shownLive.size():
 		shownLive[liveIndex].visible = false
+		shownLiveCb[liveIndex].visible = false
 	for blankIndex in shownBlank.size():
 		shownBlank[blankIndex].visible = false
+		shownBlankCb[blankIndex].visible = false
 
+	colorblind = GlobalVariables.colorblind;
+	otherShells.visible = !colorblind;
+	cbShells.visible = colorblind;
+		
 func MaybeUpdateShells() -> void:
+	if colorblind != GlobalVariables.colorblind:
+		otherShells.visible = colorblind;
+		colorblind = !colorblind;
+		cbShells.visible = colorblind;
+
 	if pastSequenceArrayLength == len(shellSpawner.sequenceArray):
 		return
+
 
 	pastSequenceArrayLength = len(shellSpawner.sequenceArray)
 
@@ -50,6 +71,8 @@ func MaybeUpdateShells() -> void:
 
 	for liveIndex in shownLive.size():
 		shownLive[liveIndex].visible = liveIndex < live
+		shownLiveCb[liveIndex].visible = liveIndex < live
 	for blankIndex in shownBlank.size():
 		shownBlank[blankIndex].visible = blankIndex < blank
+		shownBlankCb[blankIndex].visible = blankIndex < blank
 
